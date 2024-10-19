bf1e1dd9949dbd457eb9911f240dc6e74b26b3f5
hery4we <henry4we2008@gmail.com>
2024-10-19 01:44:02 +0100

    Create Security Groups for ALB, SSH, Web Server, and Database

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "vpc_cidr" {
   description = "VPC CIDR Block"
   type        = string
 }
-
+#public_subnet_az1_cidr
 variable "public_subnet_az1_cidr" {
   default     = "10.0.0.0/24"
   description = "CIDR block for Public Subnet AZ1"
@@ -12,13 +12,12 @@ variable "public_subnet_az1_cidr" {
 }
 
 
-
+#public_subnet_az2_cidr
 variable "public_subnet_az2_cidr" {
   default     = "10.0.1.0/24"
   description = "CIDR block for Public Subnet AZ2"
   type        = string
 }
-
 
 
 
@@ -29,6 +28,7 @@ variable "private_app_subnet_az1_cidr" {
   type        = string
 }
 
+#private_app_subnet_az2_cidr
 variable "private_app_subnet_az2_cidr" {
   default     = "10.0.3.0/24"
   description = "CIDR block for Private app Subnet AZ2"
@@ -37,17 +37,24 @@ variable "private_app_subnet_az2_cidr" {
 
 
 
-
+#private_data_subnet_az1_cidr
 variable "private_data_subnet_az1_cidr" {
   default     = "10.0.4.0/24"
   description = "CIDR block for Private data Subnet AZ1"
   type        = string
 }
 
-
+#private_data_subnet_az2_cidr
 variable "private_data_subnet_az2_cidr" {
   default     = "10.0.5.0/24"
   description = "CIDR block for Private data Subnet AZ2"
   type        = string
 }
 
+
+#ssh location
+variable "ssh_location" {
+  default     = "0.0.0.0/0"
+  description = "The IP address range that can SSH into the instances"
+  type        = string
+}
